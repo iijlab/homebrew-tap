66da39c5b0cc9bfbf8404c7c8e4c91734db1f560
Hajime Tazaki <thehajime@gmail.com>
2025-03-24 11:34:55 +0900

    fix lint warnings of formula
    Signed-off-by: Hajime Tazaki <thehajime@gmail.com>

--- Formula/dug.rb
+++ Formula/dug.rb
@@ -1,9 +1,6 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Dug < Formula
   desc "Extensible DNS libraries written purely in Haskell"
-  homepage ""
+  homepage "https://github.com/kazu-yamamoto/dnsext"
   url "https://github.com/kazu-yamamoto/dnsext/releases/download/dug-20250318/dug-mac-arm64"
   sha256 "516253b331396a4fc3398258728caf4c9b3bd51e5c036f0d5745fe352307e863"
   license ""
