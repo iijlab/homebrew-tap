676ba3919ce19b580320ef5aa90c7ba1e3d8bb08
Hajime Tazaki <thehajime@gmail.com>
2025-05-25 17:37:09 +0900

    fix brew test-bot issue

--- Formula/dug.rb
+++ Formula/dug.rb
@@ -1,8 +1,8 @@
 class Dug < Formula
-  version "20250318"
-  desc "Extensible DNS libraries written purely in Haskell"
+  desc "DNS lookup utility in Haskell"
   homepage "https://github.com/kazu-yamamoto/dnsext"
   url "https://github.com/kazu-yamamoto/dnsext/releases/download/dug-20250318/dug-mac-arm64"
+  version "20250318"
   sha256 "516253b331396a4fc3398258728caf4c9b3bd51e5c036f0d5745fe352307e863"
   license ""
 
