95a47c0f60af1dd7975cd65ea5953a3e4c9ee6e3
thehajime <thehajime@users.noreply.github.com>
2025-06-26 10:38:19 +0000

    Update packages

--- Formula/dug.rb
+++ Formula/dug.rb
@@ -1,9 +1,9 @@
 class Dug < Formula
   desc "DNS lookup utility in Haskell"
   homepage "https://github.com/kazu-yamamoto/dnsext"
-  url "https://github.com/kazu-yamamoto/dnsext/releases/download/dug-20250318/dug-mac-arm64"
-  version "20250318"
-  sha256 "516253b331396a4fc3398258728caf4c9b3bd51e5c036f0d5745fe352307e863"
+  url "https://github.com/thehajime/dnsext/releases/download/test/dug-mac-arm64"
+  version "test"
+  sha256 "52369d93b8de35aa39753308f3416112a7dfb987ece7b46c2f96d7b624492806"
   license ""
 
   def install
