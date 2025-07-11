4a13f170897cdfaac02c47dbdb06c0b45d3094ea
Hajime Tazaki <thehajime@gmail.com>
2025-07-11 11:58:08 +0900

    fix version name
    to use with `brew upgrade` (still testing).

--- Formula/dug.rb
+++ Formula/dug.rb
@@ -2,7 +2,7 @@ class Dug < Formula
   desc "DNS lookup utility in Haskell"
   homepage "https://github.com/kazu-yamamoto/dnsext"
   url "https://github.com/iijlab/dnsext/releases/download/pre-release-20250709/dug-mac-arm64"
-  version "pre-release-20250709"
+  version "20250709"
   sha256 "1db64a12399e542fb838580b74ad354ceeb0e8b57868ec1d41ff02f285c52ec4"
   license ""
 
