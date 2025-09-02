e27e0ecc3e4020daca13528cea481db88e2573b7
Hajime Tazaki <thehajime@gmail.com>
2025-09-02 19:04:04 +0900

    Update dug.rb

--- Formula/dug.rb
+++ Formula/dug.rb
@@ -2,7 +2,7 @@ class Dug < Formula
   desc "DNS lookup utility in Haskell"
   homepage "https://github.com/kazu-yamamoto/dnsext"
   url "https://github.com/iijlab/dnsext/releases/download/hotfix-20250901-01/dug-mac-arm64"
-  version "hotfix-20250901-01"
+  version "20250901-01"
   sha256 "b7e7cb8964deacff4e5b8dd4a4ce2cc24df6a958d59ac5c95d811a5bea89f62e"
   license ""
 
